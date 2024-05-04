249bb6d459a93089e7023c2840517d320138d49b
Doğu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2024-05-04 20:07:25 +0300

    Backend communication for student register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,36 +1,61 @@
 import React from "react";
 import classes from "./Register.module.css";
 
-class Register extends React.Component {
-  render() {
-    return (
-      <div className={classes.container}>
-        <div>
-          <h2>Register.</h2>
-          <p>You don't have an account? Sign up.</p>
-        </div>
-        <div className={classes.inputContainer}>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Surname" />
-          <input type="text" placeholder="Birth Date" />
-          <input type="text" placeholder="Student Number" />
-          <input type="text" placeholder="E-mail" />
-          <input type="text" placeholder="Password" />
-          
-          <label>
-            <input type = "checkbox"></input>
-            I agree the terms of privacy policy.
-            </label>
-        </div>
-        <div className={classes.signInContainer}>
-          <div className={classes.signInButton}>
-            <button>Register</button>
-          </div>
-          <a href="/login">Or sign in</a>
-        </div>
-      </div>
-    )
+import axios from 'axios';
+
+const Register = () => {
+  const nameRef = React.useRef(null);
+  const surnameRef = React.useRef(null);
+  const birthDateRef = React.useRef(null);
+  const studentNumberRef = React.useRef(null);
+  const emailRef = React.useRef(null);
+  const passwordRef = React.useRef(null);
+
+  async function registerRequest() {
+    try {
+      let res = await axios.post("http://localhost:9090/register/student", {
+        "studentNumber": studentNumberRef.current.value,
+        "birthDate": birthDateRef.current.value,
+        "name": nameRef.current.value,
+        "surname": surnameRef.current.value,
+        "email": emailRef.current.value,
+        "password": passwordRef.current.value
+      });
+
+      console.log("Response: " + JSON.stringify(res.data));
+    } catch (err) {
+      console.log("Error Code: " + err.response.status);
+      console.log("Error Data: " + JSON.stringify(err.response.data));
+    }
   }
+
+  return (
+    <div className={classes.container}>
+      <div>
+        <h2>Register.</h2>
+        <p>You don't have an account? Sign up.</p>
+      </div>
+      <div className={classes.inputContainer}>
+        <input ref={nameRef} id="name" type="text" placeholder="Name" />
+        <input ref={surnameRef} id="surname" type="text" placeholder="Surname" />
+        <input ref={birthDateRef} id="birthDate" type="text" placeholder="Birth Date" />
+        <input ref={studentNumberRef} id="studentNumber" type="text" placeholder="Student Number" />
+        <input ref={emailRef} id="email" type="text" placeholder="E-mail" />
+        <input ref={passwordRef} id="password" type="text" placeholder="Password" />
+        
+        <label>
+          <input type = "checkbox"></input>
+          I agree the terms of privacy policy.
+          </label>
+      </div>
+      <div className={classes.signInContainer}>
+        <div className={classes.signInButton}>
+          <button onClick={registerRequest}>Register</button>
+        </div>
+        <a href="/login">Or sign in</a>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
