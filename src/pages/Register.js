ee04c09535da6923695cb9f493ee2bb07bf0da7b
Doğu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2024-05-04 18:08:47 +0300

    Student inputs

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,29 +1,36 @@
 import React from "react";
 import classes from "./Register.module.css";
 
-const Register = () => {
-  return (
-    <div className={classes.container}>
-      <div>
-        <h2>Register.</h2>
-        <p>You don't have an account? Sign up.</p>
-      </div>
-      <div className={classes.inputContainer}>
-        <input type="text" placeholder="E-mail" />
-        <input type="text" placeholder="Password" />
-        <label>
-          <input type = "checkbox"></input>
-          I agree the terms of privacy policy.
-          </label>
-      </div>
-      <div className={classes.signInContainer}>
-        <div className={classes.signInButton}>
-          <button>Register</button>
+class Register extends React.Component {
+  render() {
+    return (
+      <div className={classes.container}>
+        <div>
+          <h2>Register.</h2>
+          <p>You don't have an account? Sign up.</p>
         </div>
-        <a href="/login">Or sign in</a>
+        <div className={classes.inputContainer}>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Surname" />
+          <input type="text" placeholder="Birth Date" />
+          <input type="text" placeholder="Student Number" />
+          <input type="text" placeholder="E-mail" />
+          <input type="text" placeholder="Password" />
+          
+          <label>
+            <input type = "checkbox"></input>
+            I agree the terms of privacy policy.
+            </label>
+        </div>
+        <div className={classes.signInContainer}>
+          <div className={classes.signInButton}>
+            <button>Register</button>
+          </div>
+          <a href="/login">Or sign in</a>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 };
 
 export default Register;
