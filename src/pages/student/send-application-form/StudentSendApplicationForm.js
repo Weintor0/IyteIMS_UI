d4a17e4b25f6a3d23f4ff388439af9ed2f62a35e
dogu <dogukocatepe@std.iyte.edu.tr>
2024-05-21 20:17:25 +0300

    Bora drag and drop fix

--- src/pages/student/send-application-form/StudentSendApplicationForm.js
+++ src/pages/student/send-application-form/StudentSendApplicationForm.js
@@ -2,30 +2,32 @@ import React, { useState } from "react";
 import classes from "./StudentSendApplicationForm.module.css";
 import ChooseCompany from "../../../components/student/ChooseCompany";
 import AddFormModal from "../../../components/student/AddFormModal";
+import Modal from "../../../components/student/Modal";
 
 const StudentSendApplicationForm = () => {
-    const [isFirstModalOpen, setFirstModalOpen] = useState(false);
-    const [isSecondModalOpen, setSecondModalOpen] = useState(false);
-  
-    const handleUploadClick = () => {
-      setFirstModalOpen(true);
-    };
-  
-    const handleContinueClick = () => {
-      setFirstModalOpen(false);
-      setSecondModalOpen(true);
-    };
-  
-    const handleCancelClick = () => {
-      setFirstModalOpen(false);
-      setSecondModalOpen(false);
-    };
-  
-    const handleSendClick = () => {
-      alert('Form Sent!');
-      setSecondModalOpen(false);
-    };
-   return (
+  const [isFirstModalOpen, setFirstModalOpen] = useState(false);
+  const [isSecondModalOpen, setSecondModalOpen] = useState(false);
+
+  const handleUploadClick = () => {
+    setFirstModalOpen(true);
+  };
+
+  const handleContinueClick = () => {
+    setFirstModalOpen(false);
+    setSecondModalOpen(true);
+  };
+
+  const handleCancelClick = () => {
+    setFirstModalOpen(false);
+    setSecondModalOpen(false);
+  };
+
+  const handleSendClick = () => {
+    alert('Form Sent!');
+    setSecondModalOpen(false);
+  };
+
+  return (
     <div className={classes.container}>
       <div className={classes.headercontainer}>
         <div>
@@ -33,27 +35,42 @@ const StudentSendApplicationForm = () => {
           <p>All application forms that have been sent so far</p>
         </div>
         <div className={classes.searchContainer}>
-            <button type="submit" className="search-button"></button>
-            <p>Name B.</p>
+          <button type="submit" className="search-button"></button>
+          <p>Name B.</p>
         </div>
       </div>
-    <div className={classes.bodyContainer}>
-      <div className={classes.listContainer}>
-        <ul><p><b className={classes.left}>Firm A</b></p><p className={classes.left}>Date a.b.c</p><a className={classes.right} href="Download">Download</a></ul>
-        <ul><p><b className={classes.left}>Firm A</b></p><p className={classes.left}>Date a.b.c</p><a className={classes.right} href="Download">Download</a></ul>
-        <ul><p><b className={classes.left}>Firm A</b></p><p className={classes.left}>Date a.b.c</p><a className={classes.right} href="Download">Download</a></ul>
-      </div>
-      <div className={classes.buttons}>
-        <button type="button" className={classes.uploadForm} onClick={handleUploadClick}>Upload New Form</button>
-        {isFirstModalOpen && (
-        <ChooseCompany onClose={handleCancelClick} onContinue={handleContinueClick} />
-    )}
-      {isSecondModalOpen && (
-        <AddFormModal onClose={handleCancelClick} onSend={handleSendClick} />
-
-      )}
-        <button type="button" className={classes.uploadForm}>Download Template</button>
-      </div>
+      <div className={classes.bodyContainer}>
+        <div className={classes.listContainer}>
+          <ul>
+            <p><b className={classes.left}>Firm A</b></p>
+            <p className={classes.left}>Date a.b.c</p>
+            <a className={classes.right} href="Download">Download</a>
+          </ul>
+          <ul>
+            <p><b className={classes.left}>Firm A</b></p>
+            <p className={classes.left}>Date a.b.c</p>
+            <a className={classes.right} href="Download">Download</a>
+          </ul>
+          <ul>
+            <p><b className={classes.left}>Firm A</b></p>
+            <p className={classes.left}>Date a.b.c</p>
+            <a className={classes.right} href="Download">Download</a>
+          </ul>
+        </div>
+        <div className={classes.buttons}>
+          <button type="button" className={classes.uploadForm} onClick={handleUploadClick}>
+            Upload New Form
+          </button>
+          {isFirstModalOpen && (
+            <ChooseCompany onClose={handleCancelClick} onContinue={handleContinueClick} />
+          )}
+          {isSecondModalOpen && (
+            <Modal showModal={isSecondModalOpen} handleClose={handleCancelClick} />
+          )}
+          <button type="button" className={classes.uploadForm}>
+            Download Template
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/student/send-application-letter/StudentSendApplicationLetter.js
+++ src/pages/student/send-application-letter/StudentSendApplicationLetter.js
@@ -1,33 +1,60 @@
 import React, { useState } from "react";
-import classes from "./StudentSendApplicationLetter.module.css";
+import classes from "./StudentSendApplicationLetter2.module.css";
+import Vector from "./images/Vector.png";
+import PlusSign from "./images/+.png";
+import Modal from "../../../components/student/Modal";
 
+const StudentSendApplicationLetter2 = () => {
+  const [showModal, setShowModal] = useState(false);
 
-const StudentSendApplicationLetter = () => {
+  const handleOpen = () => {
+    setShowModal(true);
+  };
 
-   return (
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
     <div className={classes.container}>
-      <div className={classes.headercontainer}>
+      <div className={classes.headerContainer}>
         <div>
-          <h2>Internship Oppurtunities</h2>
+          <h2>Internship Opportunities</h2>
           <p>See all your documents here.</p>
         </div>
-        <div className={classes.searchContainer}>
-            <button type="submit" className="search-button"></button>
+        <div className={classes.blackBox}>
+          <button>
+            <img src={Vector} alt="icon" />
+          </button>
+          <p>Name S.</p>
         </div>
       </div>
-    <div className={classes.bodyContainer}>
-      <div className={classes.listContainer}>
-        <ul><button></button><p>Company 1</p></ul>
-        <ul><button></button><p>Company 2</p></ul>
-
-
- 
-   
+      <div className={classes.bodyContainer}>
+        <div className={classes.header1}>
+          <ul className={classes.firstul}>
+            <button>
+              <img src={Vector} alt="icon" />
+            </button>
+            <p>Internship Advert of Firm A</p>
+          </ul>
+          <div className={classes.secondrow}>
+            <div>
+              <ul className={classes.secondul}>
+                <button className={classes.plusSign} onClick={handleOpen}>
+                  <img src={PlusSign} alt="icon" />
+                </button>
+                <p>Add summer practice application letter</p>
+              </ul>
+            </div>
+            <div className={classes.downloadbutton}>
+              <button>Download Template</button>
+            </div>
+          </div>
+        </div>
       </div>
-
-      </div>
+      {showModal && <Modal showModal={showModal} handleClose={handleClose} />}
     </div>
   );
 };
 
-export default StudentSendApplicationLetter;
+export default StudentSendApplicationLetter2;
